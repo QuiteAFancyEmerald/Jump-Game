7448bf4008c8a38c7071e583f0967b73b0a7ff49
Emerald <theemeraldhavenmmod@gmail.com>
2020-03-18 19:52:15 -0700

    Project Begin

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Basic player script. Allows for movement right, left, and up.
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+    public float movespeed; //controls speed of object.
+    public Vector2 Jumpheight;
+
+    /// <summary>
+    /// Checks for which key is being pressed, and moves based on key.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Right");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed, GetComponent<Rigidbody2D>().velocity.y); //allows the player to move right when the D key is pressed.
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Left");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(- movespeed, GetComponent<Rigidbody2D>().velocity.y); // allows the player to move left with the A key is presed.
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Jump");
+            GetComponent<Rigidbody2D>().AddForce(Jumpheight, ForceMode2D.Impulse);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/TalkingScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Script for talking to characters
+/// </summary>
+public class TalkingScript : MonoBehaviour
+{
+    public GameObject textbox;
+    public string dialogue = "Words words words."; // Words being put into the dialogue box
+    public Text targetText; // text box that is being targeted
+
+    /// <summary>
+    /// populate Target text with the words in dialogue
+    /// </summary>
+    void Start()
+    {
+        targetText.text = dialogue;
+    }
+    /// <summary>
+    /// Collision triggers population of the text box
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player")) //Player must cause the collision
+        {
+            Debug.Log("Activate text."); //Debug
+            textbox.gameObject.SetActive(true); // Spawn the text box
+        }
+    }
+}
